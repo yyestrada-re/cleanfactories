2912811876d513ca8d9975216f088048f9d1a257
yyestrada-re <yestrada.re@gmail.com>
2020-07-21 13:53:04 -0500

    skeleton done!

---
+++ script.js
@@ -0,0 +1,11 @@
+/* this code changes the navigation bar color once it reaches a certain section of the webpage */
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() >= 380) {
+      $('.navbar').css('background','black');
+    } else {
+      $('.navbar').css('background','transparent');
+      }
+  });
+  
+  
